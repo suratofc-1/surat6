49d72c2debffb024764c82bce09a436560563689
suratofc-1 <email-kamu@gmail.comgit config --global user.email apelijo010@gmail.com>
2026-07-03 19:58:37 +0700

    Setup static export untuk deploy Cloudflare

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export', // <-- Bagian ini WAJIB ada agar menghasilkan file HTML statis
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: 'export',
   images: {
-    unoptimized: true, // Tambahkan ini juga agar gambar aman di Cloudflare
+    unoptimized: true,
   },
 };
 
